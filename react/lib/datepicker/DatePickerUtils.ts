80346ddf1d0d9baaa569bd3236a37770c3b837cf
Roman Karpov <roman@romans-mbp.lan>
2024-04-08 15:12:25 -0400

    add new library dir for date picker. setup date picker utils for getting calendar dates by month and year. WIP TODO setup date picker class and functionality next

---
+++ react/lib/datepicker/DatePickerUtils.ts
@@ -0,0 +1,29 @@
+type generateCalendarProps = {
+	year: number;
+	month: number;
+};
+
+type calendarDates = Array<number | null>;
+
+/**
+  Generates calendar dates for a given year, month.
+  Fills in empty days of the week prior to first calendar day.
+ **/
+export const generateCalendar = ({ year, month }: generateCalendarProps): calendarDates => {
+	const daysInMonth = new Date(year, month + 1, 0).getDate();
+	const firstDayOfMonth = new Date(year, month, 1).getDay();
+	const calendarDates = [];
+
+	// Add empty cells for days of previous month
+	for (let i = 0; i < firstDayOfMonth; i++) {
+		calendarDates.push(null);
+	}
+
+	// Add cells for days of current month
+	for (let day = 1; day <= daysInMonth; day++) {
+		calendarDates.push(day);
+	}
+    
+    // calendarDates: [ null1, null2, ...nullFirstDayOfMonth-1, day1, ...day31 ]
+	return calendarDates; 
+};
