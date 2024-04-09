da4c2bb8b9a8f55648b53f7a2f859a582fe78b68
Roman Karpov <roman@romans-mbp.lan>
2024-04-09 12:26:44 -0400

    implement month and year selection functionality with toggle for incrementing and decrementing month. update to more semantic html tags and apply accessible roles, aria labels, etc. add header to display component and toggle section indicating selected month and year

--- react/lib/datepicker/DatePickerContainer.tsx
+++ react/lib/datepicker/DatePickerContainer.tsx
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 import { generateCalendar, calendarDates } from "./DatePickerUtils";
-import DatePickerDisplay from "./DatePickerDisplay";
+import DatePickerDisplay, {
+	PREVIOUS_MONTH,
+	NEXT_MONTH,
+} from "./DatePickerDisplay";
 
 const DatePickerContainer = () => {
 	const [selectedDate, setSelectedDate] = useState<number | null>(null);
-
-	const handleDateClick = (date: number | null) => {
-		setSelectedDate(date);
-	};
+	const [selectedMonth, setSelectedMonth] = useState<number | null>(null);
+	const [selectedYear, setSelectedYear] = useState<number | null>(null);
 
 	const currentDate = new Date();
-	const currentYear = currentDate.getFullYear();
-	const currentMonthDigit = currentDate.getMonth();
-	const currentMonthName = currentDate.toLocaleString("default", {
+	const currentYear = selectedYear === null ? currentDate.getFullYear() : selectedYear;
+	const currentMonthDigit = selectedMonth === null ? currentDate.getMonth() : selectedMonth;
+	const currentMonthName = new Date(
+		currentYear,
+		currentMonthDigit,
+		1
+	).toLocaleString("default", {
 		month: "long",
 	});
 	const calendarDates: calendarDates = generateCalendar({
@@ -20,16 +25,37 @@ const DatePickerContainer = () => {
 		month: currentMonthDigit,
 	});
 
+	const handleDateClick = (date: number | null) => {
+		setSelectedDate((prevDate) => prevDate === date ? null : date);
+	};
+
+    const handleMonthChange = (
+        direction: typeof PREVIOUS_MONTH | typeof NEXT_MONTH
+    ) => {
+        let newMonth = currentMonthDigit;
+        let newYear = currentYear;
+        if (direction === NEXT_MONTH) {
+            newMonth = currentMonthDigit === 11 ? 0 : currentMonthDigit + 1;
+            newYear = currentMonthDigit === 11 ? currentYear + 1 : currentYear;
+        } else {
+            newMonth = currentMonthDigit === 0 ? 11 : currentMonthDigit - 1;
+            newYear = currentMonthDigit === 0 ? currentYear - 1 : currentYear;
+        }
+        setSelectedMonth(newMonth);
+        setSelectedYear(newYear);
+    };
+    
 	return (
-		<>
+		<div>
 			<DatePickerDisplay
 				calendarDates={calendarDates}
 				currentMonthName={currentMonthName}
 				currentYear={currentYear}
 				onClickDate={handleDateClick}
+				onMonthChange={handleMonthChange}
 				selectedDate={selectedDate}
 			/>
-		</>
+		</div>
 	);
 };
 
--- react/lib/datepicker/DatePickerDisplay.tsx
+++ react/lib/datepicker/DatePickerDisplay.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import { calendarDates } from "./DatePickerUtils";
 
 const DATE_NAMES = ["S", "M", "T", "W", "T", "F", "S"];
+const LEFT_ARROW = '←';
+const RIGHT_ARROW = '→';
+export const PREVIOUS_MONTH = "previous";
+export const NEXT_MONTH = "next";
 
 type DatePickerDisplayProps = {
 	currentMonthName: String;
 	currentYear: number;
 	calendarDates: calendarDates;
 	onClickDate: (date: number | null) => void;
+	onMonthChange: (direction: typeof PREVIOUS_MONTH | typeof NEXT_MONTH) => void;
 	selectedDate: number | null;
 };
 
@@ -16,38 +21,63 @@ const DatePickerDisplay = ({
 	currentYear,
 	calendarDates,
 	onClickDate,
+	onMonthChange,
 	selectedDate,
 }: DatePickerDisplayProps) => {
 	return (
-		<div className="DatePicker">
-			<header className="font-bold text-3xl text-center">
-				{currentMonthName} {currentYear}
+		<>
+			<header
+				aria-label="Date Picker Header"
+				className="font-bold text-3xl text-center"
+			>
+				Single Date Picker
 			</header>
-			{/* TODO dry classname css for tailwind grid. Lookup best practices */}
-			<div className="mt-5 bg-gray-200 mt-5 grid grid-cols-7 gap-0.5 sm:gap-2 md:gap-3 lg:gap-5 xl:gap-7">
+			<div className="mt-4 px-4 flex justify-between rounded border border-gray-400">
+				<button
+					aria-label="Previous Month"
+					onClick={() => onMonthChange(PREVIOUS_MONTH)}
+					id="left"
+				>
+					<span>{LEFT_ARROW}</span>
+				</button>
+				<label aria-label="Selected Month and Year">
+					{currentMonthName} {currentYear}
+				</label>
+				<button
+					aria-label="Next Month"
+					onClick={() => onMonthChange(NEXT_MONTH)}
+					id="right"
+				>
+					<span>{RIGHT_ARROW}</span>
+				</button>
+			</div>
+			<div className="mt-2 grid grid-cols-7 gap-0.5 sm:gap-2 md:gap-3 lg:gap-5 xl:gap-7 rounded border border-gray-400">
 				{DATE_NAMES.map((day: String, idx: number) => (
 					<div
 						key={`${idx}-day`}
-						className="p-2 flex justify-center bg-gray-200"
+						className="p-2 flex justify-center text-gray-400"
+						role="columnheader"
 					>
 						{day}
 					</div>
 				))}
-			</div>
-			<div className="grid grid-cols-7 gap-0.5 sm:gap-2 md:gap-3 lg:gap-5 xl:gap-7">
 				{calendarDates.map((date: number | null, idx: number) => (
-					<div
+					<button
+						aria-label={date ? `${date}` : "Empty"}
+						aria-pressed={selectedDate === date}
 						className={`p-2 flex justify-center ${
 							date === null ? "cursor-default" : "cursor-pointer"
 						} ${date && selectedDate === date && "bg-blue-500 text-white"}`}
 						key={`${idx}-date`}
 						onClick={() => onClickDate(date)}
+						role="gridcell"
+						tabIndex={date === null ? -1 : 0}
 					>
 						{date}
-					</div>
+					</button>
 				))}
 			</div>
-		</div>
+		</>
 	);
 };
 
