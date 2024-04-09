3f735881591314c3b5ce835b932506cd8c338f7e
Roman Karpov <roman@romans-mbp.lan>
2024-04-09 19:54:51 -0400

    add test for generate calendar fn

---
+++ react/lib/datepicker/__tests__/DatePickerUtils.test.ts
@@ -0,0 +1,16 @@
+import { generateCalendar, calendarDates } from '../DatePickerUtils';
+
+describe('generateCalendar', () => {
+  it('generates calendar dates correctly for a given year and month', () => {
+    const year = 2024;
+    const month = 5;
+    const expectedDates: calendarDates = [
+        null, null, null, null, null, null, 1, 
+        2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+        14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+        24, 25, 26, 27, 28, 29, 30
+    ];
+    const result: calendarDates = generateCalendar({ year, month });
+    expect(result).toEqual(expectedDates);
+  });
+});
